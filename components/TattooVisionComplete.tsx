f4ed77e511f66f3ae5c06aa5be03d3208ced8bd5
OddSignature10857 <workspace2000.official@gmail.com>
2025-12-11 19:06:29 +0600

    faceing prompt below to edit the commit message.

--- components/TattooVisionComplete.tsx
+++ components/TattooVisionComplete.tsx
@@ -17,7 +17,7 @@ import {
   FaBolt,
   FaTrophy,
   FaHeart
-} from 'react-icons/fa';
+} from 'react-icons/fa6';
 import Image from 'next/image';
 // Emoji-Fallback für fehlende Icons
 const EmojiIcon = ({ type, className = "" }: { type: string; className?: string }) => {
