8037a71bd97e234150807a8cd49562b036df469d
OddSignature10857 <workspace2000.official@gmail.com>
2025-12-11 17:16:18 +0600

    fix

--- components/TattooVisionComplete.tsx
+++ components/TattooVisionComplete.tsx
@@ -26,6 +26,7 @@ import {
   FaTrophy,
   FaHeart
 } from 'react-icons/fa';
+import { FaPlayCircle } from "react-icons/fa";
 import Image from 'next/image';
 // Emoji-Fallback für fehlende Icons
 const EmojiIcon = ({ type, className = "" }: { type: string; className?: string }) => {
